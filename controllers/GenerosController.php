eed1e85552c3af50d19ef5b7650ae528fa721c14
Pepe Millan <jose.millan@iesdonana.org>
2018-12-03 19:26:23 +0100

    CRUD de generos

---
+++ controllers/GenerosController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\GenerosForm;
+use Yii;
+use yii\web\NotFoundHttpException;
+
+/**
+ * Definición del controlador generos.
+ */
+class GenerosController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        $filas = \Yii::$app->db
+            ->createCommand('SELECT * FROM generos')->queryAll();
+        return $this->render('index', [
+            'filas' => $filas,
+        ]);
+    }
+
+    public function actionCreate()
+    {
+        $generosForm = new GenerosForm();
+
+        if ($generosForm->load(Yii::$app->request->post()) && $generosForm->validate()) {
+            Yii::$app->db->createCommand()
+                ->insert('generos', $generosForm->attributes)
+                ->execute();
+            Yii::$app->session->setFlash('success', 'Fila insertada correctamente.');
+            return $this->redirect(['generos/index']);
+        }
+        return $this->render('create', [
+            'generosForm' => $generosForm,
+        ]);
+    }
+
+    public function actionUpdate($id)
+    {
+        $generosForm = new GenerosForm(['attributes' => $this->buscarGenero($id)]);
+
+        if ($generosForm->load(Yii::$app->request->post()) && $generosForm->validate()) {
+            Yii::$app->db->createCommand()
+                ->update('generos', $generosForm->attributes, ['id' => $id])
+                ->execute();
+            return $this->redirect(['generos/index']);
+        }
+
+        return $this->render('update', [
+            'generosForm' => $generosForm,
+            'listaGeneros' => $this->listaGeneros(),
+        ]);
+    }
+
+    public function actionDelete($id)
+    {
+        Yii::$app->db->createCommand()->delete('generos', ['id' => $id])->execute();
+        return $this->redirect(['generos/index']);
+    }
+
+    private function listaGeneros()
+    {
+        $generos = Yii::$app->db->createCommand('SELECT * FROM generos')->queryAll();
+        $listaGeneros = [];
+        foreach ($generos as $genero) {
+            $listaGeneros[$genero['id']] = $genero['genero'];
+        }
+        return $listaGeneros;
+    }
+
+    private function buscarGenero($id)
+    {
+        $fila = Yii::$app->db
+            ->createCommand('SELECT *
+                               FROM generos
+                              WHERE id = :id', [':id' => $id])->queryOne();
+        if ($fila === false) {
+            throw new NotFoundHttpException('Ese género no existe.');
+        }
+        return $fila;
+    }
+}
